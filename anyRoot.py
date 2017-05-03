90f80a6555cbd627f02bb962e19efb4905379eeb
willnt <billyrnt@gmail.com>
2017-05-03 16:49:16 -0400

    Create anyRoot.py

---
+++ anyRoot.py
@@ -0,0 +1,8 @@
+def findRoot(root, num):
+	for n in range(num):
+		if n ** root == num:
+			print(n)
+
+
+##This will run however i want to find a way to make calculate range in a way where i can get a float return.
+  
